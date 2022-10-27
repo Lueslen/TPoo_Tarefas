66d7eb8102f0fe70ad7bb3210725dc0b4212ca4a
Lueslen Machado da Silva <lueslensilva@hotmail.com>
2022-10-27 10:54:17 -0300

    Update Janela.java

--- Codigo/Janela.java
+++ Codigo/Janela.java
@@ -1,22 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
 
-
-
 public class Janela extends JFrame {
 
     private JTextArea tarefaTextArea;
     private JLabel labelTarefa;
-
     private JLabel labelPrioridade;
     private String[] idades = {"Alta", "Media", "Baixa"};
     private JComboBox priorBox;
-
     private JCheckBox finalizado;
-
     private JButton cancelar;
     private JButton salvar;
-
 
     public Janela(){
         JFrame frame = new JFrame();
@@ -35,29 +29,19 @@ public class Janela extends JFrame {
         panel.setLayout(layout);
         layout.setAutoCreateGaps(true);
         layout.setAutoCreateContainerGaps(true);
-
-
-
-
+        
         labelTarefa = new JLabel("Tarefa");
         tarefaTextArea = new JTextArea(4,6);
         //tarefaTextArea.setSize(200,10);
         tarefaTextArea.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         tarefaTextArea.setLineWrap(true);
-
-
-
+        
         labelPrioridade = new JLabel("Prioridade");
         priorBox = new JComboBox(idades);
-
-
         finalizado = new JCheckBox("Finalizado");
-
-
         cancelar = new JButton("Cancelar");
-
         salvar = new JButton("Salvar");
-
+        
         layout.setVerticalGroup(
                 layout.createSequentialGroup()
                         .addComponent(labelTarefa)
@@ -68,8 +52,6 @@ public class Janela extends JFrame {
                         .addGroup(layout.createParallelGroup()
                             .addComponent(cancelar)
                             .addComponent(salvar))
-
-
 
         );
         layout.setHorizontalGroup(
@@ -82,7 +64,6 @@ public class Janela extends JFrame {
                         .addGroup(layout.createSequentialGroup().addContainerGap(300,300)
                             .addComponent(cancelar)
                             .addComponent(salvar))
-
 
         );
 
